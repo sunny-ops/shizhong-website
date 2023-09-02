05a30b2c0b25e5d4ba7e8713b0424379e5a7a9f4
shizhong <shizhong1989@gmail.com>
2023-09-02 15:03:16 -0700

    Add Navbar

---
+++ src/components/MyNavbar.js
@@ -0,0 +1,61 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./MyNavbar.css";
+
+function MyNavbar() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Sunny</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <div class="app__navbar-menu">
+            <svg
+              stroke="currentColor"
+              fill="white"
+              stroke-width="0"
+              viewBox="0 0 20 20"
+              height="30px"
+              width="30px"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 7a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 13a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          {/* <svg
+            stroke="currentColor"
+            fill="white"
+            stroke-width="0"
+            viewBox="0 0 20 20"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3 7a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 13a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clip-rule="evenodd"
+            ></path>
+          </svg> */}
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto " style={{ marginLeft: "auto" }}>
+            <Nav.Link href="#home">HOME</Nav.Link>
+            <Nav.Link href="#link">ABOUT</Nav.Link>
+            <Nav.Link href="#work">WORK</Nav.Link>
+            <Nav.Link href="#skills">SKILLS</Nav.Link>
+            <Nav.Link href="#contact">CONTACT</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default MyNavbar;
