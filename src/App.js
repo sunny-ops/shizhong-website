d378f1c39caed5c647b139a4a509109f21415c34
shizhong <shizhong1989@gmail.com>
2023-09-02 18:29:17 -0700

    Add Home.js and Home.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import MyNavbar from "./components/MyNavbar";
+import Home from "./components/Home";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div>
       <MyNavbar />
-    </>
+      <Home />
+    </div>
   );
 }
 
--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -28,21 +28,6 @@ function MyNavbar() {
               ></path>
             </svg>
           </div>
-          {/* <svg
-            stroke="currentColor"
-            fill="white"
-            stroke-width="0"
-            viewBox="0 0 20 20"
-            height="1em"
-            width="1em"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 7a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 13a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            ></path>
-          </svg> */}
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto " style={{ marginLeft: "auto" }}>
